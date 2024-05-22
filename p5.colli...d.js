4c2e7462b4e8ab2b349d154f636bcb9eaeeb4199
cachorro <00001077581063sp@al.educacao.sp.gov.br>
2024-05-22 11:35:15 -0300

    Create p5.colli...d.js

---
+++ p5.colli...d.js
@@ -0,0 +1,24 @@
+  }
+  // point
+  var point = this.createVector(px, py);
+  // arc center point
+  var arcPos = this.createVector(ax, ay);
+  // arc radius vector
+  var radius = this.createVector(arcRadius, 0).rotate(arcHeading);
+
+  var pointToArc = point.copy().sub(arcPos);
+
+  if (point.dist(arcPos) <= (arcRadius + buffer)) {
+    var dot = radius.dot(pointToArc);
+    var angle = radius.angleBetween(pointToArc);
+    if (dot > 0 && angle <= arcAngle / 2 && angle >= -arcAngle / 2) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// p5.vector version of collidePointArc
+p5.prototype.collidePointArcVector = function(p1, a, arcRadius, arcHeading, arcAngle, buffer){
+  return p5.prototype.collidePointArc(p1.x, p1.y, a.x, a.y, arcRadius, arcHeading, arcAngle, buffer);
+}
